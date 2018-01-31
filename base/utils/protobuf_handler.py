1be39b3d800dee7169102b1ace88811883fce79d
wang.ruyi <wang.ruyi@qq.com>
2018-01-31 16:09:51 +0800

    Update protobuf_handler.py

--- base/utils/protobuf_handler.py
+++ base/utils/protobuf_handler.py
@@ -41,3 +41,50 @@ def get_message_handle(msgType):
     else:
         logger.error("msgType:%s not exist in HandleDict" % msgType)
         return None
+
+def decode_all_data(requestHandler):
+    dataBuffer = bytes()
+    while True:
+        try:
+            data = requestHandler.recv(AgentChannelProto.MAX_LENGTH)
+            if not data:
+                logger.info("connection closed")
+                return None
+
+            dataBuffer += data
+            while True:
+                if len(dataBuffer) < AgentChannelProto.MIN_LENGTH:
+                    logger.error("length of input string < %d" % AgentChannelProto.MIN_LENGTH)
+                    break
+                elif len(dataBuffer) > AgentChannelProto.MAX_LENGTH:
+                    logger.error("length of input string > %d" % AgentChannelProto.MAX_LENGTH)
+                    return None
+                signature = socket.ntohl(struct.unpack('I',dataBuffer[0:4])[0])
+                if signature != AgentChannelProto.SIGNATURE:
+                    logger.error("signature is wrong. expected '%s',acture '%s'" %
+                                      (AgentChannelProto.SIGNATURE, signature))
+                    return None
+                version = socket.ntohs(struct.unpack('H',dataBuffer[8:10])[0])
+                if version != AgentChannelProto.VERSION:
+                    logger.error("version is wrong. expected '%s',acture '%s'" %
+                                      (AgentChannelProto.VERSION, version))
+                    return None
+                pbLen = socket.ntohl(struct.unpack('I',dataBuffer[4:8])[0])
+                if len(dataBuffer) < (pbLen + AgentChannelProto.MIN_LENGTH):
+                    logger.info("recv data:%s is not enough: %s" % ((len(dataBuffer)), (pbLen + AgentChannelProto.MIN_LENGTH)))
+                    break
+                msgType = socket.ntohs(struct.unpack('H',dataBuffer[10:12])[0])
+                logger.info("pblen:%s,msgType:%s" % (pbLen,  msgType))
+                pbBuf = dataBuffer[12:12 + pbLen]
+                msg = get_message_class(msgType)
+                if msg is None:
+                    return None
+                try:
+                    msg.ParseFromString(pbBuf) #if parse expction?
+                except pbmsg.Error:
+                    logger.error("Parse Error!")
+                    return None
+                return msgType, msg
+        except Exception, err:
+            logger.error("decode_all_data err %s" % traceback.format_exc(err))
+            return None
