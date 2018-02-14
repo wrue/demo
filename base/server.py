3b7b4ee4e8cbe3913dc45907730ca8a8a9a23239
wang.ruyi <wang.ruyi@qq.com>
2018-02-14 09:26:40 +0800

    Update server.py

--- base/server.py
+++ base/server.py
@@ -15,6 +15,20 @@ class LocalServer(SocketServer.ThreadingTCPServer):
         SocketServer.ThreadingTCPServer.__init__(self, server_address, RequestHandlerClass)
         self.data_manager = data_manager
         self.is_waiting_to_shutdown = False
+        
+    def get_request(self):
+        try:
+            newsocket, fromaddr = self.socket.accept()
+            connstream = ssl.wrap_socket(newsocket,
+                                         server_side=True,
+                                         ssl_version=ssl.PROTOCOL_TLSv1_2,
+                                         cert_reqs=ssl.CERT_REQUIRED,
+                                         certfile="",
+                                         keyfile="",
+                                         ca_certs="")
+            return connstream, fromaddr
+        except Exception, e:
+            raise socket.error
 
     def destroy_self(self):
         self.shutdown()
